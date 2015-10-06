85b26936ba7a6e6bbf3991956b68f9e0fe98abf9
Phoenix Logan <Phoenix@PhoenixLogan.net>
2015-10-06 09:20:23 -0500

    made changes, more coming later

--- cluster_script_final.R
+++ cluster_script_final.R
@@ -5,25 +5,19 @@
 library(xcms)
 library(pvclust)
 
-args <- 
-  
-  myDir = "C:/Users/bugnilab/Desktop/Strep_Cdfs/MarineStreps"
+# command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+myDir = args[1]
 setwd(myDir)
-(WD <- getwd())
 
-xset <- xcmsSet(scanrange= c(245, 1685))
-xsg1 <- group(xset, method = "mzClust", mzppm = 20, mzabs =0, minsamp =3, minfrac =0)
-
-
-
-
-xsg2 <- fillPeaks(xsg1)
-
-values <- groupval(xsg2, value = "into")
-
-for (r in 1:ncol(values))
-  values[,r] <- values[,r] / sum(values[,r])
-
+run_xcms <- function() {
+  xset <- xcmsSet(scanrange= c(245, 1685))
+  xsg1 <- group(xset, method = "mzClust", mzppm = 20, mzabs =0, minsamp =3, minfrac =0)
+  xsg2 <- fillPeaks(xsg1)
+  values <- groupval(xsg2, value = "into")
+  for (r in 1:ncol(values))
+    values[,r] <- (values[,r] / sum(values[,r]))
+}
 
 fit <-pvclust(values, method.dist = "cor", nboot=1000)
 plot(fit, main = "Hierarchal Cluster of Marine Streptomyces", print.pv = FALSE, print.num=FALSE)
