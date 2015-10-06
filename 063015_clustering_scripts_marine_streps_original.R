6cfab09f132fa0c91deb7f1cedc52dc6c3c9095b
Phoenix Logan <Phoenix@PhoenixLogan.net>
2015-10-06 09:03:28 -0500

    Chris script that I am cleaning up for him

---
+++ 063015_clustering_scripts_marine_streps_original.R
@@ -0,0 +1,29 @@
+#Author Chris Thomas
+## THIS SCRIPT GENERATES Marine Streptomyces STRAINS CLUSTER
+
+
+library(xcms)
+library(pvclust)
+
+args <- 
+
+myDir = "C:/Users/bugnilab/Desktop/Strep_Cdfs/MarineStreps"
+setwd(myDir)
+(WD <- getwd())
+
+xset <- xcmsSet(scanrange= c(245, 1685))
+xsg1 <- group(xset, method = "mzClust", mzppm = 20, mzabs =0, minsamp =3, minfrac =0)
+
+
+
+
+xsg2 <- fillPeaks(xsg1)
+
+values <- groupval(xsg2, value = "into")
+
+for (r in 1:ncol(values))
+  values[,r] <- values[,r] / sum(values[,r])
+
+
+fit <-pvclust(values, method.dist = "cor", nboot=1000)
+plot(fit, main = "Hierarchal Cluster of Marine Streptomyces", print.pv = FALSE, print.num=FALSE)
