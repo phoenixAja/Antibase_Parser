db162569c90370be11a2b9d6751bbf66bb7ce905
phoenixAja <phoenix@phoenixlogan.net>
2015-10-09 10:31:03 -0500

    added png capabilities

--- cluster_script_final.R
+++ cluster_script_final.R
@@ -6,18 +6,23 @@ library(xcms)
 library(pvclust)
 
 # command line arguments
+# set working directory
 args <- commandArgs(trailingOnly = TRUE)
 myDir = args[1]
 setwd(myDir)
 
 run_xcms <- function() {
+  # Analyze data and return graph
   xset <- xcmsSet(scanrange= c(245, 1685))
   xsg1 <- group(xset, method = "mzClust", mzppm = 20, mzabs =0, minsamp =3, minfrac =0)
   xsg2 <- fillPeaks(xsg1)
   values <- groupval(xsg2, value = "into")
   for (r in 1:ncol(values))
     values[,r] <- (values[,r] / sum(values[,r]))
+  fit <-pvclust(values, method.dist = "cor", nboot=1000)
+  png(file=plot_title, units = "in", width=11, height=8.5, res=300)
+  plot(fit, main = "Hierarchal Cluster of Marine Streptomyces", print.pv = FALSE, print.num=FALSE)
+  dev.off()
 }
 
-fit <-pvclust(values, method.dist = "cor", nboot=1000)
-plot(fit, main = "Hierarchal Cluster of Marine Streptomyces", print.pv = FALSE, print.num=FALSE)
+run_xcms()
