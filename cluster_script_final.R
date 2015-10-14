e7b5c3ccb5be1d7b8e411ff507631b2716320d48
Phoenix Logan <Phoenix@PhoenixLogan.net>
2015-10-14 09:39:24 -0500

    added eps, pdf, plot title

--- cluster_script_final.R
+++ cluster_script_final.R
@@ -17,12 +17,28 @@ run_xcms <- function(plot_title) {
   xsg1 <- group(xset, method = "mzClust", mzppm = 20, mzabs =0, minsamp =3, minfrac =0)
   xsg2 <- fillPeaks(xsg1)
   values <- groupval(xsg2, value = "into")
+  
   for (r in 1:ncol(values))
     values[,r] <- (values[,r] / sum(values[,r]))
   fit <-pvclust(values, method.dist = "cor", nboot=1000)
+  
+  # print png file
   png(file=paste0(plot_title, "png"), units = "in", width=11, height=8.5, res=300)
-  plot(fit, main = "Hierarchal Cluster of Marine Streptomyces", print.pv = FALSE, print.num=FALSE)
+  plot(fit, main=paste0(plot_title, "Hierarchal Cluster of Marine Streptomyces"), 
+       print.pv = FALSE, print.num=FALSE)
   dev.off()
+  # print eps file
+  setEPS()
+  postscript(paste0(plot_title, ".eps"))
+  plot(fit, main=paste0(plot_title, "Hierarchal Cluster of Marine Streptomyces"),
+       print.pv = FALSE, print.num=FALSE)
+  dev.off()
+  # print pdf file
+  pdf(file = paste0(plot_title, ".pdf"))
+  plot(fit, main = paste0(plot_title, "Hierarchal Cluster of Marine Streptomyces"), 
+       print.pv = FALSE, print.num=FALSE)
+  dev.off()
+  
   }
 
 run_xcms(args[2])
